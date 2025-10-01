af237fea1f77a5b4dad61430b61961d634434f52
Sean Murphy <sean@iamseanmurphy.com>
2025-10-01 07:28:29 +0000

    4.4.40

--- astra-sites/inc/lib/onboarding/assets/dist/onboarding/main.asset.php
+++ astra-sites/inc/lib/onboarding/assets/dist/onboarding/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-media-utils', 'wp-url'), 'version' => '38c7e8c88e09ec77dd60');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-media-utils', 'wp-url'), 'version' => 'de99a4b0698dd6d261d7');
